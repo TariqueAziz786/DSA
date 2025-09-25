94dfc44d161565333bd27b0d4a16f622bdae81e1
Tarique Aziz Khan <tariqueazizkhan660@gmail.com>
2025-09-25 15:24:36 +0530

    first commit

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,23 @@
+public class linearSearch {
+
+    public static int linear(int numbers[], int key) {
+        for(int i=0; i<numbers.length;i++) {
+            if(numbers[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10,12, 14,16};
+        int key = 10;
+
+        int result = linear(numbers, key);
+        if (result == -1) {
+            System.out.println("element not found");
+        } else {
+            System.out.println("element found at index: " + result);
+        }
+    }
+}
